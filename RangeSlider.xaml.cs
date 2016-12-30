6e7dac326f46fbc6d54d843dc1ae98c9d34843c4
Phpdevpad <chibox@gmail.com>
2016-12-30 10:53:24 +0100

    Initial release

---
+++ RangeSlider.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+//http://www.thejoyofcode.com/Creating_a_Range_Slider_in_WPF_and_other_cool_tips_and_tricks_for_UserControls_.aspx
+namespace BetterControl
+{
+    /// <summary>
+    /// Interaction logic for RangeSlider.xaml
+    /// </summary>
+    public partial class RangeSlider : UserControl
+    {
+        //https://msdn.microsoft.com/en-us/library/ms742215(v=vs.110).aspx
+        public delegate void MyControlEventHandler(object sender, double args);
+        public event MyControlEventHandler OnLowerSlider_ValueChanged;
+        public event MyControlEventHandler OnUpperSlider_ValueChanged;        
+       
+        public RangeSlider()
+        {
+            InitializeComponent();            
+            this.Loaded += Slider_Loaded;
+        }
+
+        void Slider_Loaded(object sender, RoutedEventArgs e)
+        {
+            LowerSlider.ValueChanged += LowerSlider_ValueChanged;
+            UpperSlider.ValueChanged += UpperSlider_ValueChanged;
+        }
+
+        private void LowerSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpperSlider.Value = Math.Max(UpperSlider.Value, LowerSlider.Value);
+
+            if (OnLowerSlider_ValueChanged != null)
+                OnLowerSlider_ValueChanged(this, LowerSlider.Value);
+        }
+
+        private void UpperSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            LowerSlider.Value = Math.Min(UpperSlider.Value, LowerSlider.Value);
+
+            if (OnUpperSlider_ValueChanged != null)
+                OnUpperSlider_ValueChanged(this, UpperSlider.Value);
+        }
+
+        public double Minimum
+        {
+            get { return (double)GetValue(MinimumProperty); }
+            set { SetValue(MinimumProperty, value); }
+        }
+
+        public static readonly DependencyProperty MinimumProperty =
+            DependencyProperty.Register("Minimum", typeof(double), typeof(RangeSlider), new UIPropertyMetadata(0d));
+
+        public double LowerValue
+        {
+            get { return (double)GetValue(LowerValueProperty); }
+            set { SetValue(LowerValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty LowerValueProperty =
+            DependencyProperty.Register("LowerValue", typeof(double), typeof(RangeSlider), new UIPropertyMetadata(0d));
+
+        public double UpperValue
+        {
+            get { return (double)GetValue(UpperValueProperty); }
+            set { SetValue(UpperValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty UpperValueProperty =
+            DependencyProperty.Register("UpperValue", typeof(double), typeof(RangeSlider), new UIPropertyMetadata(0d));
+
+        public double Maximum
+        {
+            get { return (double)GetValue(MaximumProperty); }
+            set { SetValue(MaximumProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaximumProperty =
+            DependencyProperty.Register("Maximum", typeof(double), typeof(RangeSlider), new UIPropertyMetadata(1d));
+
+        
+
+
+    }
+}
